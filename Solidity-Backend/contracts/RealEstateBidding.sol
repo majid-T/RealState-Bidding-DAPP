acd959ecb50e0096a8c75a2161656c605a936031
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-06-17 13:40:02 +0430

    Added aditi ERC721 test cases on backend

--- Solidity-Backend/contracts/RealEstateBidding.sol
+++ Solidity-Backend/contracts/RealEstateBidding.sol
@@ -4,4 +4,10 @@ pragma solidity ^0.6.0;
 
 import "./ERC721.sol";
 
-contract RealEstateBidding {}
+contract RealEstateBidding is ERC721 {
+    constructor() public ERC721("RealEstateBidding", "REB") {}
+
+    function mint(uint256 propID) public {
+        _mint(msg.sender, propID);
+    }
+}
