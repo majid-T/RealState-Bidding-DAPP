fbc7d3c7dfe4ea3e5f3b8b70a1e989483ef75393
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-06-18 00:41:49 +0430

    Added some functions on smart contract from Malhar

--- Solidity-Backend/contracts/RealEstateBidding.sol
+++ Solidity-Backend/contracts/RealEstateBidding.sol
@@ -7,20 +7,23 @@ import "./ERC721.sol";
 contract RealEstateBidding is ERC721 {
     // struct for each bid
     struct Bid {
+        address bidAddress;
         uint256 bidAmount;
-        address bidder;
     }
+
     //Struct for each bid process
     struct SingleBidProcess {
         address realtor;
         bool onMarket;
         Bid highestBid;
-        Bid[] allBids;
         bool winnerRevealed;
+        address winner;
     }
 
+    //tokens On market
+    uint256[] public market;
     //mapping from token Ids to bid process
-    mapping(uint256 => SingleBidProcess) private allBids;
+    mapping(uint256 => SingleBidProcess) allBids;
 
     constructor() public ERC721("RealEstateBidding", "REB") {}
 
@@ -43,9 +46,9 @@ contract RealEstateBidding is ERC721 {
         allBids[tokenId] = SingleBidProcess(
             realtor,
             false,
-            Bid(0, address(0)),
-            new Bid[](0),
-            false
+            Bid(address(0), 0),
+            false,
+            address(0)
         );
     }
 
@@ -53,19 +56,81 @@ contract RealEstateBidding is ERC721 {
     function putOnMarket(uint256 tokenId) public {
         //check if token exists
         //check if sender is the assigned realtor
+        require(
+            msg.sender == allBids[tokenId].realtor,
+            "RealStatebidding: Only assigned realtor can call this function."
+        );
         //change on market to true on bidProcess struct
+        allBids[tokenId].onMarket = true;
+        market.push(tokenId);
         // Fire event on market
+        // Event Firing ---->
     }
 
     //biding on properties
     function bid(uint256 tokenId, uint256 amount) public {
         //check if tokenId is still on market
+        require(
+            allBids[tokenId].onMarket,
+            "RealStatebidding:Property is no longer on Market"
+        );
         //check if amount is higher than highest bid
+        require(
+            allBids[tokenId].highestBid.bidAmount < amount,
+            "RealStatebidding:There is already a highest bid on this property"
+        );
         //replace highest bid with new bid
+        allBids[tokenId].highestBid = Bid(msg.sender, amount);
         //push bid to array bids
         //Fire new high bid
+        // Event Firing ---->
     }
 
     // Reveal winner function
-    //TBD other functions
+    function revealWinner(uint256 tokenId) public returns (address) {
+        // Check is caller the realtor
+        require(
+            msg.sender == allBids[tokenId].realtor,
+            "RealStatebidding: Only assigned realtor can call this function."
+        );
+
+        // check if there is a bid on property
+        if (allBids[tokenId].highestBid.bidAmount == 0) {
+            return address(0);
+        } else {
+            allBids[tokenId].onMarket = false;
+            allBids[tokenId].winnerRevealed = true;
+            allBids[tokenId].winner = allBids[tokenId].highestBid.bidAddress;
+            return allBids[tokenId].winner;
+            //Fire new high bid
+            // Event Firing ---->
+        }
+    }
+
+    // function view Bids on my property
+    function getHighestBid(uint256 tokenId) public view returns (uint256) {
+        return allBids[tokenId].highestBid.bidAmount;
+    }
+
+    // function get market size
+    function getMarketSize() public view returns (uint256) {
+        //Can only view bids On my property
+        return market.length;
+    }
+
+    // function to withdraw property from market
+    function withdrawFromMarket(uint256 tokenId) public returns (bool) {
+        //Can only wwithdraw for my property
+        require(
+            ownerOf(tokenId) == msg.sender,
+            "RealStatebidding: Can not withdraw others token"
+        );
+
+        allBids[tokenId].onMarket = false;
+        allBids[tokenId].realtor = address(0);
+
+        //Fire event property withdraw from market
+        // Fire Event ----->
+        return true;
+    }
 }
